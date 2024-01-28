05468e4b374840767c24b92c8051a17a5d654539
Carlos Santos <casantosmu@gmail.com>
2024-01-28 21:23:44 +0100

    Initial commit

---
+++ FindTheOddInt/Kata.cs
@@ -0,0 +1,18 @@
+﻿namespace FindTheOddInt;
+
+public static class Kata
+{
+    public static int FindInt(int[] seq)
+    {
+        var odds = new HashSet<int>();
+        foreach (var digit in seq)
+        {
+            if (!odds.Remove(digit))
+            {
+                odds.Add(digit);
+            }
+        }
+
+        return odds.Single();
+    }
+}
---
+++ Tests/FindTheOddInt.cs
@@ -0,0 +1,18 @@
+namespace Tests;
+
+using global::FindTheOddInt;
+
+public class FindTheOddInt
+{
+    [Theory]
+    [InlineData(new[] { 7 }, 7)]
+    [InlineData(new[] { 0 }, 0)]
+    [InlineData(new[] { 1, 1, 2 }, 2)]
+    [InlineData(new[] { 0, 1, 0, 1, 0 }, 0)]
+    [InlineData(new[] { 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 }, 4)]
+    public void TestOddOccurrence(int[] input, int expected)
+    {
+        var result = Kata.FindInt(input);
+        Assert.Equal(expected, result);
+    }
+}
